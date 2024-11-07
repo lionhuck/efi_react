c2aad52788e3bced78951ece52ac49041ad4fefc
Leon Federico Huck <leonhuck007@gmail.com>
2024-11-07 20:35:14 -0300

    Conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import UsersContainer from './Components/Users/UsersContainer'
-<<<<<<< HEAD
-import { Menubar } from 'primereact/menubar';
-import CreateUser from './Components/Users/CreateUsers'
-import LoginUser from './Components/Users/LoginUser'
-import CreateEquipo from './Components/Equipos/CreateEquipo';
-=======
 import CreateUser from './Components/Users/CreateUsers'
 import LoginUser from './Components/Users/LoginUser'
 import CreateEquipo from './Components/Equipos/CreateEquipo'
@@ -14,18 +8,13 @@ import { Menubar } from 'primereact/menubar';
 import EquiposContainer from './Components/Equipos/EquiposContainer'
 
 
->>>>>>> 15356c6 (ROUTES)
 function App() {
   const items = [
     {label: 'Usuarios', icon:'pi pi-users', url:'/usuarios'},
     {label: 'CargarUsuario', icon:'pi pi-users', url:'/nuevo-usuario'},
     {label: 'Login', icon:'pi pi-users', url:'/inicio-sesion'},
-<<<<<<< HEAD
-    {label: 'Equipos', icon:'pi pi-users', url:'/nuevo-equipo'},    
-=======
     {label: 'Nuevo Equipo', icon:'pi pi-users', url:'/nuevo-equipo'},
     {label: 'Equipos', icon:'pi pi-users', url:'/equipos'},    
->>>>>>> 15356c6 (ROUTES)
   ]
   return (
     <BrowserRouter>
@@ -36,10 +25,7 @@ function App() {
         <Route path='/nuevo-usuario' element={<CreateUser/>}></Route>
         <Route path='/inicio-sesion' element={<LoginUser/>}></Route>
         <Route path='/nuevo-equipo' element={<CreateEquipo/>}></Route>
-<<<<<<< HEAD
-=======
         <Route path='/equipos' element={<EquiposContainer/>}></Route>
->>>>>>> 15356c6 (ROUTES)
       </Routes>
     </BrowserRouter>
   )
