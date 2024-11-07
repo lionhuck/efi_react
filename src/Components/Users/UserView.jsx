bd0cc32f573b8d253c0b00c1d13cb90d871b2446
Leon Federico Huck <leonhuck007@gmail.com>
2024-11-07 20:58:20 -0300

    esto VAAAAAAAAA

--- src/Components/Users/UserView.jsx
+++ src/Components/Users/UserView.jsx
@@ -1,132 +1,41 @@
-import { Fragment, useState } from "react"
+import { Fragment } from "react"
 import { ProgressSpinner } from "primereact/progressspinner"
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from "primereact/button";
-import { Dialog } from "primereact/dialog";
-import { ToggleButton } from 'primereact/togglebutton';
-import { Formik } from "formik";
-import * as Yup from 'yup'
+        
 
-const UsersView = ({ loadingUsers, dataUsers }) => {
-    const token = JSON.parse(localStorage.getItem('token'))
 
-    const [openDialogEditUser, setOpenDialogEditUser] = useState(false)
-    const [editUser, setEditUser] = useState({})
 
-    const bodyIsAdmin = (rowData) => {
+const UsersView = ({loadingData, data}) => {
+    const bodyIsAdmin = (rowData) =>{
         return (
-            rowData.is_admin ? <span>Si</span> : <span>No</span>
+            rowData.is_admin === true ? <span>SI</span> : <span>NO</span>
         )
     }
 
-    const bodyActions = (rowData) => {
-        return (
+    const bodyActions = (row) => {
+        return(
             <div>
-                <Button icon='pi pi-pencil' label='Editar' onClick={() => (setEditUser(rowData), setOpenDialogEditUser(true))} />
-                <Button icon='pi pi-trash' label='Borrar' onClick={() => (onDeletUser(rowData))} />
+                <Button label="Delete" />
+                <Button label="Update"/>
             </div>
         )
     }
 
-    const ValidationSchema = Yup.object().shape({
-        username: Yup.string()
-            .required('Este campo es requerido')
-            .max(50, 'El username no debe ser mayor a 50 caracteres'),
-    })
-
-
-    const onEditUser = async (values) => {
-        const bodyEditUser = {
-            username: values.username,
-            is_admin: values.is_admin
-        }
-
-        const response = await fetch(`http://127.0.0.1:5000/users/${editUser.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(bodyEditUser),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        })
-
-
-    }
-
-    const onDeletUser = async (values) => {
-        const response = await fetch(`http://127.0.0.1:5000/users/${values.id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        })
-
-
-    }
 
     return (
         <Fragment>
-            {loadingUsers ?
-                <ProgressSpinner />
-                :
-                <DataTable value={dataUsers} tableStyle={{ minWidth: '50rem' }}>
-                    <Column field="username" header="Nombre de usuario"></Column>
-                    <Column field="is_admin" body={bodyIsAdmin} header="¿Es administrador?"></Column>
-                    <Column body={bodyActions} header="Acciones"></Column>
-                </DataTable>
-            }
-            <Dialog
-                visible={openDialogEditUser}
-                onHide={() => setOpenDialogEditUser(false)}
-                header='Editar usuario'
-            >
-                <Formik
-                    initialValues={{ is_admin: editUser.is_admin, username: editUser.username }}
-                    validationSchema={ValidationSchema}
-                >
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        isSubmitting,
-                        isValid
-                        /* and other goodies */
-                    }) => (
-                        <form style={{ display: 'inline-grid' }}>
-                            <label>
-                                Nombre de usuario
-                            </label>
-                            <input
-                                type="text"
-                                name="username"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.username}
-                            />
-                            {errors.username && touched.username && errors.username}
-                            <label>
-                                ¿Es administrador?
-                            </label>
-                            <ToggleButton
-                                name='is_admin'
-                                checked={values.is_admin}
-                                onChange={handleChange}
-                                onLabel="Si"
-                                offLabel="No"
-                            />
-                            <button type="button" onClick={() => onEditUser(values)} disabled={values.password === '' || values.username === '' || !isValid}>
-                                Modificar usuario
-                            </button>
-                        </form>
-                    )}
-                </Formik>
-
-            </Dialog>
-        </Fragment>
+        {loadingData ? <ProgressSpinner/> 
+        : 
+        <DataTable value={data} tableStyle={{ minWidth: '50rem' }}>
+            <Column field="nombre" header="Username"></Column>
+            <Column field="is_admin" body={bodyIsAdmin} header="Admin"></Column>
+            <Column body={bodyActions} header="Botones"></Column>
+        </DataTable>
+        }
+    </Fragment>
     )
+
 }
 export default UsersView
