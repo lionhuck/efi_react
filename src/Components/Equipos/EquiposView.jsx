cebe9006bfca2ba7b9d9b5d7c7e42e9fdbf36d6c
Gonzalo Toledo <g.toledo@itecriocuarto.org.ar>
2024-11-12 09:11:33 -0300

    editar equipo

--- src/Components/Equipos/EquiposView.jsx
+++ src/Components/Equipos/EquiposView.jsx
@@ -1,18 +1,75 @@
-import { Fragment } from "react";
-import { ProgressSpinner } from "primereact/progressspinner";
+import { Fragment, useState } from "react"
+import { ProgressSpinner } from "primereact/progressspinner"
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { ToggleButton } from 'primereact/togglebutton';
+import { Formik } from "formik";
+import * as Yup from 'yup'
 
-const EquiposView = ({ loadingData, data, handleDelete, handleUpdate }) => {
-    const bodyActions = (row) => {
+
+// const EquiposView = ({ loadingData, data, handleDelete, handleUpdate }) => {
+
+const EquiposView = ({ loadingData, data}) => {
+    const token = JSON.parse(localStorage.getItem('token'))
+    
+    const [openDialogEditEquipo, setOpenDialogEditEquipo] = useState(false)
+
+    const [editEquipo, setEditEquipo] = useState({})
+
+
+
+    const bodyActions = (rowData) => {
         return (
             <div style={{ display: 'flex', gap: '0.5rem' }}>
-                <Button label="Delete" onClick={() => handleDelete(row.id)} />
-                <Button label="Update" onClick={() => handleUpdate(row.id)} />
-            </div>
+            <Button label="Editar" onClick={() => { setEditEquipo(rowData); setOpenDialogEditEquipo(true); }} />
+            <Button label="Delete" onClick={() => onDeleteEquipo(rowData)} />
+        </div>
         );
     };
+
+
+    // const bodyActions = (row) => {
+    //     return (
+    //         <div style={{ display: 'flex', gap: '0.5rem' }}>
+    //             <Button label="Update" onClick={() => handleUpdate(row.id)} />
+    //             <Button label="Delete" onClick={() => handleDelete(row.id)} />
+                
+    //         </div>
+    //     );
+    // };
+
+    const onEditEquipo = async (values) => {
+        const bodyEditEquipo = {
+            nombre: values.nombre,
+            costo: values.costo
+        }
+
+        const response = await fetch(`http://localhost:5000/editar/${editEquipo.id}/equipos`, {
+            method: 'PUT',
+            body: JSON.stringify(bodyEditEquipo),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+
+
+    }
+
+    const onDeleteEquipo = async (values) => {
+        const response = await fetch(`http://localhost:5000/editar/${values.id}/equipos`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+
+
+    }
+
 
     return (
         <Fragment>
@@ -28,6 +85,64 @@ const EquiposView = ({ loadingData, data, handleDelete, handleUpdate }) => {
             ) : (
                 <p>No hay datos disponibles.</p>
             )}
+
+            {openDialogEditEquipo&&(
+            <Dialog
+                visible={openDialogEditEquipo}
+                onHide={() => setOpenDialogEditEquipo(false)}
+                header='Editar Equipo'
+            >
+                <Formik
+                    initialValues={{ costo: editEquipo.costo, nombre: editEquipo.nombre }}
+                    // validationSchema={ValidationSchema}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        isSubmitting,
+                        isValid
+                        /* and other goodies */
+                    }) => (
+                        <form style={{ display: 'inline-grid' }}>
+                            <label>
+                                Nombre del equipo
+                            </label>
+                            <input
+                                type="text"
+                                name="nombre"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.nombre}
+                            />
+                            {errors.nombre && touched.nombre && errors.nombre}  
+
+                            <label>
+                                Costo
+                            </label>
+                            <input
+                                type="number"
+                                name="costo"
+                                step={"0.01"}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.costo}
+                            />
+                            {errors.costo && touched.costo && errors.costo}
+                            
+                            <button type="button" onClick={() => onEditEquipo(values)} disabled={!values.nombre || !values.costo || !isValid}>
+                                Modificar Equipo
+                            </button>
+                        </form>
+                    )}
+                </Formik>
+
+            </Dialog>
+            )}
+
+
         </Fragment>
     );
 };
